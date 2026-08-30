c5a36594424863d58f8de4d3afac6309ec0ae17a
Vikas Kalyan <64362404+vikaskalyan-rgb@users.noreply.github.com>
2026-08-30 17:19:57 +0530

    Import WeeklyLogPage and add route for it

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,10 @@ import { AppShell } from "@/components/layout/AppShell";
 import { AppProvider } from "@/context/AppContext";
 import { DashboardPage } from "@/pages/DashboardPage";
 import { TasksPage } from "@/pages/TasksPage";
+import { WeeklyLogPage } from "@/pages/WeeklyLogPage";
 import { GeneratePdfPage } from "@/pages/GeneratePdfPage";
 import { RecognitionPage } from "@/pages/RecognitionPage";
-import { SettingsPage } from "./pages/SettingsPage";
+import { SettingsPage } from "@/pages/SettingsPage";
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <Route element={<AppShell />}>
             <Route index element={<DashboardPage />} />
             <Route path="tasks" element={<TasksPage />} />
+            <Route path="weekly-log" element={<WeeklyLogPage />} />
             <Route path="recognition" element={<RecognitionPage />} />
             <Route path="generate" element={<GeneratePdfPage />} />
             <Route path="settings" element={<SettingsPage />} />
